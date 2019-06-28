1f3561eb4f646b0536586279b59be4f8e8e5bbba
danil <danila-fomm@mail.ru>
2019-06-29 00:21:49 +0300

    HW12 upd 3.1

--- src/com/urise/webapp/MainStream.java
+++ src/com/urise/webapp/MainStream.java
@@ -11,17 +11,22 @@ public class MainStream {
 
         List<Integer> list = Arrays.asList(15, 20, 18, 17, 21); //91
         List<Integer> list1 = Arrays.asList(8, 19, 40, 17, 20); //104
-        System.out.println(oddOrEven(list1));
         System.out.println(oddOrEven(list));
+        System.out.println(oddOrEven(list1));
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
         int finalCounter = integers.stream().reduce(0, Integer::sum);
-        return integers.stream().filter(x -> (finalCounter % 2 > 0) == (x % 2 > 0)).collect(Collectors.toList());
+        return integers.stream()
+                .filter(x -> (finalCounter % 2 > 0) != (x % 2 > 0))
+                .collect(Collectors.toList());
     }
 
     private static int minValue(int[] values) {
-        return Arrays.stream(values).sorted().distinct().reduce(0, (x, y) ->
+        return Arrays.stream(values)
+                .sorted()
+                .distinct()
+                .reduce(0, (x, y) ->
                 x * 10 + y);
     }
 
